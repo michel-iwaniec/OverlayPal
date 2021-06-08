f9919898959250513d602583961f4f0aba42d780
MichelIwaniec <michel.iwaniec@gmail.com>
2021-06-08 22:23:55 +0100

    Fix OverlayPal not working when run from a directory with spaces: * Add function quoteString to put a string in double-quotes * Make OverlayOptimiser::runCmplProgram quote filename parameters when calling process

--- src/cpp/OverlayOptimiser.cpp
+++ src/cpp/OverlayOptimiser.cpp
@@ -31,6 +31,13 @@
 
 //---------------------------------------------------------------------------------------------------------------------
 
+std::string quoteString(const std::string& s)
+{
+    return std::string("\"") + s + std::string("\"");
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+
 OverlayOptimiser::OverlayOptimiser():
     mBackgroundColor(0)
 {
@@ -134,8 +141,8 @@ void OverlayOptimiser::runCmplProgram(const std::string& inputFilename,
     // Execute process
     std::string cmplExecutable("Cmpl/bin/cmpl");
     std::string params;
-    params += " -i " + outputFilename;
-    params += " -solutionCsv " + solutionCsvFilename;
+    params += " -i " + quoteString(outputFilename);
+    params += " -solutionCsv " + quoteString(solutionCsvFilename);
     int exitCode = executeProcess(exePathFilename(cmplExecutable), params, timeOut);
     if(exitCode != 0)
     {
